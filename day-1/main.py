81a5ace564f9d6ca35dd1fe12eb5572d842166c5
matthew.hendrie <matthew.hendrie@thebluerock.com.au>
2024-12-02 11:48:17 +1100

    refactored solution

--- day-1/main.py
+++ day-1/main.py
@@ -8,8 +8,8 @@ right_ids = []
 
 for x in input:
     ids = x.split(' ')
-    left_ids.append(ids[0])
-    right_ids.append(ids[3])
+    left_ids.append(int(ids[0]))
+    right_ids.append(int(ids[3]))
 
 # PART 1
 left_ids.sort()
@@ -17,17 +17,13 @@ right_ids.sort()
 
 total_distance = 0
 for i in range(len(left_ids)):
-    total_distance += abs(int(left_ids[i]) - int(right_ids[i]))
+    total_distance += abs(left_ids[i] - right_ids[i])
 
 print(total_distance)
 
 # PART 2
 total_similarity = 0
 for i in range(len(left_ids)):
-    occur = 0
-    for x in range(len(right_ids)):
-        if left_ids[i] == right_ids[x]:
-            occur += 1
-    total_similarity += int(left_ids[i]) * occur
+    total_similarity += right_ids.count(left_ids[i]) * left_ids[i]
 
 print(total_similarity)
