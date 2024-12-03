bb23fac19ce2d64889a42968c1b83056ef390277
matthew.hendrie <matthew.hendrie@thebluerock.com.au>
2024-12-03 11:53:59 +1100

    finished day 2, needs to be refactored

--- day-2/main.py
+++ day-2/main.py
@@ -1,3 +1,5 @@
+from tabnanny import check
+
 import numpy as np
 
 input = []
@@ -5,21 +7,43 @@ with open('input.txt', 'r') as f:
     for line in f:
         input.append(line.strip())
 
-# PART 1
-safe_levels = 0
-for x in input:
-    levels = x.split(' ')
-    levels = list(map(int, levels))
+def check_levels(diff):
     current_safe = True
-    diff = np.diff(levels)
-    # check if diff only contains positives or negatives
     if any(diff < 0) and any(diff > 0):
         current_safe = False
     for i in diff:
         if abs(i) < 1 or abs(i) > 3:
             current_safe = False
             break
-    if current_safe:
-        safe_levels += 1
+    return current_safe
 
-print(safe_levels)
+# PART 1
+safe_count = 0
+for x in input:
+    levels = x.split(' ')
+    levels = list(map(int, levels))
+    diff = np.diff(levels)
+    if check_levels(diff):
+        safe_count += 1
+
+print(safe_count)
+
+# PART 2
+fixed_safe_count = 0
+for x in input:
+    levels = x.split(' ')
+    levels = list(map(int, levels))
+    fail_count = 0
+    diff = np.diff(levels)
+    if check_levels(diff):
+        fixed_safe_count += 1
+    else:
+        # Remove current level and try again
+        for i in range(len(levels)):
+            temp = levels.copy()
+            temp.pop(i)
+            diff = np.diff(temp)
+            if check_levels(diff):
+                fixed_safe_count += 1
+                break
+print(fixed_safe_count)
