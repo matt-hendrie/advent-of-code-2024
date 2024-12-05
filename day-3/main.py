887953a30c32b79e4293b446f87504b61365a86d
matthew.hendrie <matthew.hendrie@thebluerock.com.au>
2024-12-06 08:07:27 +1100

    day 3 part 2

--- day-3/main.py
+++ day-3/main.py
@@ -6,6 +6,7 @@ with open('input.txt', 'r') as f:
         input.append(line.strip())
 
 regex_match = r'mul\(\d{1,3},\d{1,3}\)'
+combined_pattern = r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)"
 
 # PART 1
 total = 0
@@ -14,4 +15,20 @@ for x in input:
     for match in matches:
         numbers = re.findall(r'\d{1,3}', match)
         total += int(numbers[0]) * int(numbers[1])
+print(total)
+
+# PART 2
+process = True
+total = 0
+for x in input:
+    matches = re.finditer(combined_pattern, x)
+    for match in matches:
+        if match.group() == 'do()':
+            process = True
+        elif match.group() == "don't()":
+            process = False
+        else:
+            numbers = re.findall(r'\d{1,3}', match.group())
+            if process:
+                total += int(numbers[0]) * int(numbers[1])
 print(total)
